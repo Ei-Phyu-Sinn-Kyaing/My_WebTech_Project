7c9d5053bb22ee72f26755602335a125c0f6e0a8
Ei-Phyu-Sinn-Kyaing <40804629@live.napier.ac.uk>
2026-04-25 20:15:37 +0630

    Add files via upload

--- sound-manager.js
+++ sound-manager.js
@@ -127,19 +127,18 @@ function startMusic() {
     if (!isMuted) {
         const currentPage = window.location.pathname.split("/").pop() || "index.html";
         const newSrc = pageAudioMap[currentPage] || "sounds/theme.mp3";
-        const absoluteNewSrc = window.location.origin + "/" + newSrc;
 
         const lastSrc = sessionStorage.getItem('audioLastSrc');
         const lastTime = sessionStorage.getItem('audioLastTime');
         
-        if (bgAudio.src === absoluteNewSrc) {
+        if (bgAudio.src.endsWith(newSrc)) {
             if (bgAudio.paused) bgAudio.play();
             return;
         }
 
         bgAudio.src = newSrc;
 
-        if (lastSrc === absoluteNewSrc && lastTime) {
+        if (lastSrc && lastSrc.endsWith(newSrc) && lastTime) {
             bgAudio.currentTime = parseFloat(lastTime);
         }
 
